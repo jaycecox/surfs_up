6c254b1f09cb6d3202ece68f7f26e288b24074cb
jaycecox <92542382+jaycecox@users.noreply.github.com>
2021-12-16 19:06:32 -0500

    '

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-pip install Flask
-pip install psycopg2-binary
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+from flask import Flask , jsonify
+import datetime as dt 
